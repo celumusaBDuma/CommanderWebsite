279a9e27ea37f99a3648f400734f21dc98a0665f
LuckyMaley <Langelihlelucky.mhlongo@gmail.com>
2020-06-10 10:44:16 +0200

    Add project files.

---
+++ CommanderWebsite/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CommanderWebsite.Startup))]
+namespace CommanderWebsite
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
